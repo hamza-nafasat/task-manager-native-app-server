bbab4f40a66b82c87f9f0c44031d166063a820db
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-04-28 18:10:13 +0500

    change route names

--- routers/User.js
+++ routers/User.js
@@ -25,15 +25,15 @@ router.route("/verify").post(isAuthenticated, verify);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 
-router.route("/newtask").post(isAuthenticated, addTask);
+router.route("/new-task").post(isAuthenticated, addTask);
 router.route("/me").get(isAuthenticated, getMyProfile);
 
 router.route("/task/:taskId").get(isAuthenticated, updateTask).delete(isAuthenticated, removeTask);
 
-router.route("/updateprofile").put(isAuthenticated, singleUpload, updateProfile);
-router.route("/updatepassword").put(isAuthenticated, updatePassword);
+router.route("/update-profile").put(isAuthenticated, singleUpload, updateProfile);
+router.route("/update-password").put(isAuthenticated, updatePassword);
 
-router.route("/forgetpassword").post(forgetPassword);
-router.route("/resetpassword").put(resetPassword);
+router.route("/forget-password").post(forgetPassword);
+router.route("/reset-password").put(resetPassword);
 
 export default router;
